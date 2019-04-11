a92803f6fedefd48a1cfca04e4e87a621c5392b8
Toshiuk <nara_toshiraka@hotmail.com>
2019-04-11 00:45:06 -0300

    Config server

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-nwedj.mongodb.net/omnistack?retryWrites=true', {
+    useNewUrlParser: true
+});
 
-})
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
